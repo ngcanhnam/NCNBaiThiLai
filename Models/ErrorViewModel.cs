30ab26b444706e63ab65869688005e2524022346
ngcanhnam <ngcanhnam123@gmail.com>
2024-06-20 21:11:46 +0700

    commit note

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -6,3 +6,5 @@ public class ErrorViewModel
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
+
+//dotnet aspnet-codegenerator controller -name LophocController -m lophoc -dc LTQLDb --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries -sqlite
